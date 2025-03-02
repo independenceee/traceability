f7e4d9196632712dcf101ca85c20ccaceced5dee
Nguyễn Khánh <108068667+independenceee@users.noreply.github.com>
2025-03-03 00:11:00 +0700

    fix: update txbuilder file and convert blockfrost api connect to mesh

---
+++ contract/scripts/constants/index.ts
@@ -0,0 +1,24 @@
+import { Network } from "@meshsdk/core";
+const BLOCKFROST_API_KEY = process.env.BLOCKFROST_API_KEY || "";
+const KOIOS_TOKEN = process.env.KOIOS_TOKEN || "";
+
+const appNetwork: Network = (process.env.NEXT_PUBLIC_APP_NETWORK?.toLowerCase() as Network) || "preprod";
+
+const appNetworkId = appNetwork === "mainnet" ? 1 : 0;
+const IPFS_ENDPOINT = process.env.IPFS_ENDPOINT || "";
+const IPFS_GATEWAY = process.env.NEXT_PUBLIC_IPFS_GATEWAY || "https://ipfs.io/";
+export const APP_WALLET_ADDRESS = process.env.APP_WALLET_ADDRESS || "";
+export const EXCHANGE_FEE_PRICE = process.env.EXCHANGE_FEE_PRICE || "1000000"; //lovelace
+
+export const MINT_REFERENCE_SCRIPT_HASH = process.env.MINT_REFERENCE_SCRIPT_HASH || "";
+export const STORE_REFERENCE_SCRIPT_HASH = process.env.STORE_REFERENCE_SCRIPT_HASH || "";
+
+export const title = {
+  mint: "mint.mint.mint",
+  store: "store.store.spend",
+};
+
+
+
+
+export { appNetwork, appNetworkId, BLOCKFROST_API_KEY, KOIOS_TOKEN, IPFS_ENDPOINT, IPFS_GATEWAY };
---
+++ contract/scripts/utils/index.ts
@@ -0,0 +1,68 @@
+import cbor from "cbor";
+import axios from "axios";
+/**
+ * @description Convert inline datum from utxo to metadata
+ * 1. Converts a hex string into a buffer for decoding.
+ * 2. Decodes CBOR data from the buffer to a JavaScript object.
+ * 3. Outputs a JSON metadata ready for further use
+ *
+ * @param datum
+ * @returns metadata
+ */
+export async function datumToJson(
+  datum: string,
+  option?: {
+    contain_pk?: boolean;
+  },
+): Promise<unknown> {
+  const cborDatum: Buffer = Buffer.from(datum, "hex");
+  const datumMap = (await cbor.decodeFirst(cborDatum)).value[0];
+  if (!(datumMap instanceof Map)) {
+    throw new Error("Invalid Datum");
+  }
+  const obj: Record<string, string> = {};
+  datumMap.forEach((value, key) => {
+    const keyStr = key.toString("utf-8");
+    if (keyStr === "_pk" && !option?.contain_pk) {
+      return;
+    }
+    obj[keyStr] = keyStr !== "_pk" ? value.toString("utf-8") : value.toString("hex");
+  });
+  return obj;
+}
+
+export async function getPkHash(datum: string) {
+  const cborDatum: Buffer = Buffer.from(datum, "hex");
+  const decoded = await cbor.decodeFirst(cborDatum);
+  for (const [key, value] of decoded.value[0]) {
+    if (key.toString("utf-8") === "_pk") {
+      return value.toString("hex");
+    }
+  }
+  return null;
+}
+
+export const parseError = (error: unknown): string => {
+  if (error instanceof Error) {
+    return error.message;
+  }
+  return JSON.stringify(error);
+};
+
+export const parseHttpError = (error: unknown): string => {
+  if (axios.isAxiosError(error)) {
+    if (error.response) {
+      return JSON.stringify({
+        data: error.response.data,
+        headers: error.response.headers,
+        status: error.response.status,
+      });
+    } else if (error.request && !(error.request instanceof XMLHttpRequest)) {
+      return JSON.stringify(error.request);
+    } else {
+      return JSON.stringify({ code: error.code, message: error.message });
+    }
+  } else {
+    return JSON.stringify(error);
+  }
+};
