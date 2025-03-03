b92cfa0ee87ceea207cb7bfcdc2c3a51eacc61e4
Nguyễn Khánh <108068667+independenceee@users.noreply.github.com>
2025-03-04 05:21:17 +0700

    develop: loading component with animation time for interface

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -1,18 +1,27 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                background: "var(--background)",
+                foreground: "var(--foreground)",
+            },
+        },
+        animation: {
+            scale: "scaleAnimation 1.3s ease-in-out infinite",
+        },
+        keyframes: {
+            scaleAnimation: {
+                "0%, 70%, 100%": { transform: "scale(1)" },
+                "35%": { transform: "scale(0)" },
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 } satisfies Config;
