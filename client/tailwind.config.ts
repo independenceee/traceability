9404969e68a2699ae6f820c16d8cb0ae7550462f
Nguyễn Khánh <108068667+independenceee@users.noreply.github.com>
2025-03-12 09:41:56 +0700

    feat: remove tailwind-scrollbar plugin from configuration

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -71,5 +71,5 @@ export default {
             },
         },
     },
-    plugins: [require("tailwindcss-animate"), require("tailwind-scrollbar")],
+    plugins: [require("tailwindcss-animate")],
 } satisfies Config;
