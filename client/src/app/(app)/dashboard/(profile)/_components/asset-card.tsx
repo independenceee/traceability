af4017f81ac90a1dd8b8619aab79ea7735af9d38
Nguyen Duy Khanh <nguyenkhanh17112003@gmail.com>
2025-04-10 22:28:21 +0700

    fix: Adjust padding in AssetCard component for improved layout

--- client/src/app/(app)/dashboard/(profile)/_components/asset-card.tsx
+++ client/src/app/(app)/dashboard/(profile)/_components/asset-card.tsx
@@ -17,11 +17,11 @@ export default function AssetCard({ data }: { data: AssetDetails }) {
   return (
     <Link href={`/dashboard/asset/${policy_id + asset_name}`}>
       <div className="rounded-lg shadow-none transition-shadow duration-300 hover:shadow-md hover:shadow-slate-800">
-        <Card className="h-full p-2">
+        <Card className="h-full ">
           <AspectRatio ratio={4 / 3} className="bg-muted">
             <FileDisplay src={imgSrc} alt={"image"} type={mediaType} className="h-full w-full rounded-t-lg object-cover" />
           </AspectRatio>
-          <div className="flex flex-col items-center justify-start gap-2 self-stretch px-4 py-2">
+          <div className="flex flex-col items-center justify-start p-2 gap-2 self-stretch px-4 py-2">
             <div className="font-semibold self-stretch text-center text-base text-ellipsis overflow-hidden whitespace-nowrap">{assetNameString}</div>
             <div className="font-medium self-stretch text-center text-sm text-secondary text-ellipsis overflow-hidden whitespace-nowrap">
               {fingerprint}
