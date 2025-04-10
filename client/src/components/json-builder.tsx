e1ab469c861599aedd2782321f31e83c94d2aa60
Nguyen Duy Khanh <nguyenkhanh17112003@gmail.com>
2025-04-11 00:42:07 +0700

    feat: Refactor JsonBuilder component layout and replace Trash icon with FaTrash

--- client/src/components/json-builder.tsx
+++ client/src/components/json-builder.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { FaTrash } from "react-icons/fa";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
@@ -8,21 +9,21 @@ import { cn } from "@/utils";
 import { FilePick } from "./file-pick";
 import { useJsonBuilderStore } from "./store";
 import { isEmpty } from "lodash";
-import { Trash } from "lucide-react";
+import { Card } from "./ui/card";
 
 export default function JsonBuilder({ className }: { className?: string }) {
   const { fields, addField, removeField, updateField, error, getJsonResult } = useJsonBuilderStore();
   return (
-    <div className={cn(className, "flex h-full bg-section p-5")}>
-      <div className="w-1/2 p-4 space-y-6">
-        <div className="flex flex-wrap items-center justify-between gap-2 rounded-lg p-2 mb-4">
+    <div className={cn(className, "flex h-full bg-section gap-4 flex-1")}>
+      <Card className="w-1/2 p-4 flex-1 space-y-2 flex flex-col">
+        <div className="flex flex-wrap items-center justify-between gap-2 rounded-lg ">
           <h1 className="text-2xl font-semibold tracking-tight">Metadata Builder</h1>
         </div>
-        <div className="h-4"> {error && <p className="text-red-500">{error}</p>}</div>
-        <div>
+        <div className=" ont-semibold tracking-tight"> {error && <p className="text-red-500">{error}</p>}</div>
+        <div className="flex-1">
           {!isEmpty(fields) &&
             fields.map((field, index) => (
-              <div key={index} className="flex items-center space-x-2 mb-4">
+              <div key={index} className="flex items-center space-x-2 mb-2">
                 <div className="flex-1">
                   <Label htmlFor={`key-${index}`} className="sr-only">
                     Key
@@ -46,19 +47,21 @@ export default function JsonBuilder({ className }: { className?: string }) {
                   />
                 </div>
                 <Button variant="destructive" className="text-white" onClick={() => removeField && removeField(index)}>
-                  <Trash size={20} />
+                  <FaTrash size={20} />
                 </Button>
               </div>
             ))}
-          <div className="flex items-center justify-start space-x-4">
-            <Button onClick={addField}>Add Field</Button>
-            <FilePick />
-          </div>
         </div>
-      </div>
-      <div className="w-1/2 p-4 ">
-        <Textarea className="w-full h-full resize-none font-mono" value={JSON.stringify(getJsonResult(), null, 2)} readOnly />
-      </div>
+        <div className="flex items-center justify-start space-x-4">
+          <Button className="w-full self-stretch" onClick={addField}>
+            Add Field
+          </Button>
+          <FilePick />
+        </div>
+      </Card>
+      <Card className="w-1/2 p-4 h-full flex-1 flex">
+        <Textarea className="w-full h-full resize-none font-mono " value={JSON.stringify(getJsonResult(), null, 2)} readOnly />
+      </Card>
     </div>
   );
 }
